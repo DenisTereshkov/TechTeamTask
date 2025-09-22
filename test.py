03142fbc1b85414a832e9c3a534ee74d56d8fe3e
Dennis Tereshkow <tereshkowdennis@gmail.com>
2025-09-22 19:33:59 +0300

    fix: update answers logic

--- test.py
+++ test.py
@@ -9,13 +9,13 @@ def store_periods(group):
     Промопериодом мы считаем непрерывный(!) отрезок времени,
     когда были продажи в рамках одного магазина.
     """
-    promo_periods_df = group.sort_values('period_id')
-    promo_periods_df['is_new_period'] = (
-        (promo_periods_df['period_id'] - promo_periods_df['period_id'].shift(1)) != 1
+    promo_periods = group.sort_values('period_id')
+    promo_periods['is_new_period'] = (
+        (promo_periods['period_id'] - promo_periods['period_id'].shift(1)) != 1
     )
-    periods = promo_periods_df['is_new_period'].cumsum()
-    promo_periods_df['period_group'] = periods
-    return promo_periods_df
+    periods = promo_periods['is_new_period'].cumsum()
+    promo_periods['period_group'] = periods
+    return promo_periods
 
 
 def continuous_period_by_store(df):
@@ -30,7 +30,9 @@ def continuous_period_by_store(df):
 def median_duration_periods(df):
     """Медиана продолжительности промопериода (количество недель)."""
     median_period_by_store = df.groupby('store_id').apply(
-        lambda group: store_periods(group).groupby('period_group').size().median(),
+        lambda group: store_periods(group).groupby(
+            'period_group'
+        ).size().median(),
         include_groups=False
     )
     return median_period_by_store.median()
@@ -39,7 +41,9 @@ def median_duration_periods(df):
 def sales_by_store_by_period(df):
     """Объемы продаж по промопериодам."""
     median_period_by_store = df.groupby('store_id').apply(
-        lambda group: store_periods(group).groupby('period_group')['sales_volume'].median(),
+        lambda group: store_periods(group).groupby(
+            'period_group'
+        )['sales_volume'].median(),
         include_groups=False
     )
     return median_period_by_store
@@ -48,7 +52,7 @@ def sales_by_store_by_period(df):
 def median_periods_by_store(df):
     """Медиану количества промопериодов на один магазин"""
     median_period_by_store = df.groupby('store_id').apply(
-        lambda group: store_periods(group).groupby('period_group').size(),
+        lambda group: store_periods(group)['period_group'].nunique(),
         include_groups=False
     )
     return median_period_by_store.median()
@@ -56,19 +60,30 @@ def median_periods_by_store(df):
 
 def display():
     print(60*'-')
-    print(f'Общее количество промопериодов (во всех магазинах): {continuous_period_by_store(df)}')
+    print(
+        'Общее количество промопериодов (во всех магазинах): '
+        f'{continuous_period_by_store(df)}'
+    )
     print(60*'-')
-    print(f'Медиана продолжительности промопериода (количество недель): {median_duration_periods(df):.0f}')
+    print(
+        'Медиана продолжительности промопериода (количество недель): '
+        f'{median_duration_periods(df):.0f}'
+    )
     print(60*'-')
     medians = sales_by_store_by_period(df)
     print("Объемы продаж по промопериодам:")
     medians = sales_by_store_by_period(df)
     for (store_id, period_group), median_value in medians.items():
-        print(f'Магазин: {store_id}.Промопериод: {period_group}. Объем продаж: {median_value:.2f}')
+        print(
+            f'Магазин: {store_id}.Промопериод: {period_group}. '
+            f'Объем продаж: {median_value:.2f}'
+        )
     print(60*'-')
-    print(f'Медиана количества промопериодов: {median_periods_by_store(df)}')
+    print(
+        'Медиана количества промопериодов на один магазин: '
+        f'{median_periods_by_store(df)}'
+    )
     print(60*'-')
-
 
 
 if __name__ == '__main__':
